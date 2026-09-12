824a7ac3fa2380af8f7be7bebf56394473c41212
codecoemsbackup <codecoems@gmail.com>
2026-09-12 10:47:53 -0500

    Update index.ts
    fix owner

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+import { storage } from "./storage";
 
 const app = express();
 
@@ -48,6 +49,8 @@ app.use((req, res, next) => {
 });
 
 (async () => {
+  await storage.ensureServer();
+
   const server = await registerRoutes(app);
 
   app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
